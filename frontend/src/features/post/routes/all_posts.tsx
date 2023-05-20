f1904ecbaf1e87ef6b1b28d435dedd1b8f0d9cb1
Takayasu Beharu <info@jumpeer.jp>
2023-05-20 12:36:47 +0900

    [DONE] filtered posts done

--- frontend/src/features/post/routes/all_posts.tsx
+++ frontend/src/features/post/routes/all_posts.tsx
@@ -2,13 +2,44 @@ import { PostList } from "@/components/post_list";
 import { PostUser } from "@/types";
 import { useLoaderData } from "react-router-dom";
 
+import { useState } from "react";
+
 export function AllPosts(): JSX.Element {
   const { posts } = useLoaderData() as { posts: PostUser[] };
+
+  const [filteredPosts, setfilteredPosts] = useState(posts);
+
+  const handleSearchInput = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const value = e.target.value.toLowerCase();
+    const filteredPosts = posts.filter((post) => {
+      return (
+        post.user.name.toLowerCase().includes(value) ||
+        post.content.toLowerCase().includes(value)
+      );
+    });
+    setfilteredPosts(filteredPosts);
+  };
+
   return (
     <>
       <div className="main posts-index">
         <div className="container">
-          <PostList posts={posts} />
+          <div className="searchForm__container"
+          style={{ margin: "0 auto 20px" }}>
+            <form
+            className="searchForm__form"
+            method="POST">
+              <input
+              type="text"
+              className="searchForm__input"
+              data-test="search-input"
+              onChange={handleSearchInput}
+              style={{ width: "40%", padding: "0.5em", border: "1px solid #999", color: "#555" }}
+              />
+            </form>
+          </div>
+
+          <PostList posts={filteredPosts} />
         </div>
       </div>
     </>
